6c644bf5ff787f56859cd7a1253c2eefeb914e9b
Gaëtan Staquet <gaetan.staquet@gmail.com>
2023-10-08 18:01:00 +0200

    Generate academic part from JSON files

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import pathlib
 import builder
-
+import cvbuilder
+import cvbuilder.contexts.latex
+import cvbuilder.contexts.html # Temporary workaround for a bug in the library
+from cvbuilder.contexts.markdown import MarkdownContext
+from cvbuilder.modules.contact import ContactModule
+from cvbuilder.modules.logos import LogosModule
+from cvbuilder.modules.award import AwardModule
+from cvbuilder.modules.event import EventModule
+from cvbuilder.modules.job import JobModule
+from cvbuilder.modules.language import LanguageModule
+from cvbuilder.modules.project import ProjectModule
+from cvbuilder.modules.publication import PublicationModule
+from cvbuilder.modules.summary import SummaryModule
+from cvbuilder.modules.supervision import SupervisionModule
+from cvbuilder.modules.talk import TalkModule
+from cvbuilder.modules.teach import TeachModule
+from cvbuilder.modules.text import LinkModule, TextModule
 
 def remove_dir(directory: pathlib.Path):
     if not directory.exists():
@@ -12,12 +28,54 @@ def remove_dir(directory: pathlib.Path):
             remove_dir(child)
     directory.rmdir()
 
+# From resume.json
+cv_builder = cvbuilder.Builder()
 
+cv_builder.add_context(MarkdownContext("sources/academic/index.md", "Academic CV"))
+
+cv_builder.add_module("logos", LogosModule())
+cv_builder.add_module("summary", SummaryModule())
+cv_builder.add_module("jobs", JobModule())
+cv_builder.add_module("awards", AwardModule())
+cv_builder.add_module(None, LinkModule("Publications", "My list of publications is given on a ", "publications.md", "specific page", ".", level=1))
+cv_builder.add_module("projects", ProjectModule())
+cv_builder.add_module(None, LinkModule("Talks and events", "The list of attended events and given talks is given on a ", "talks.md", "specific page", ".", level=1))
+cv_builder.add_module(None, LinkModule("Teaching duties and supervision", "The list of my attending duties and supervised projects can be consulted on a ", "teaching.md", "specific page", ".", level=1))
+cv_builder.add_module("languages", LanguageModule())
+cv_builder.add_module(None, TextModule("Miscellaneous", "During the academic year 2016-2017, I won the 454th place (out of 5558) in University CodeSprint, and my team won the 43d place in the Benelux Algorithm Programming Contest (BAPC).", level=1))
+cv_builder.add_module("contact", ContactModule())
+
+cv_builder.build("resume/resume.json")
+
+cv_builder = cvbuilder.Builder()
+cv_builder.add_context(MarkdownContext("sources/academic/talks.md", "Given talks and attended events"))
+
+cv_builder.add_module("talks", TalkModule())
+cv_builder.add_module("events", EventModule())
+
+cv_builder.build("resume/talks_events.json")
+
+cv_builder = cvbuilder.Builder()
+cv_builder.add_context(MarkdownContext("sources/academic/publications.md", "Publications"))
+
+cv_builder.add_module("publications", PublicationModule())
+
+cv_builder.build("resume/publications.json")
+
+cv_builder = cvbuilder.Builder()
+cv_builder.add_context(MarkdownContext("sources/academic/teaching.md", "Teaching duties and supervision"))
+
+cv_builder.add_module("teaching", TeachModule())
+cv_builder.add_module("supervision", SupervisionModule())
+
+cv_builder.build("resume/teaching.json")
+
+# Markdown -> HTML
 input_folder = pathlib.Path("sources/")
 output_folder = pathlib.Path("output/")
 global_setup = builder.Global(
     title="Gaëtan Staquet",
-    base_url="https://docskellington.github.io/",
+    base_url="file:///home/gaetan/GitHub/Perso/docskellington.github.io/output/",
     links_in_header=[
         ("Home page", "index.html"),
         ("Academic CV", "academic/index.html"),
--- sources/academic/index.md
+++
@@ -1,4 +0,0 @@
-title: Academic Curriculum Vitae
-
-This part is under work.
-For now, please head to [https://informatique.umons.ac.be/staff/Staquet.Gaetan/](https://informatique.umons.ac.be/staff/Staquet.Gaetan/).
